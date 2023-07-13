730ca48b6c5e4f8425e91cf81a84f5bf8ba95151
Sarathi <Sarathik86@gmail.com>
2023-07-13 17:23:27 -0500

    issue with tc 5

--- src/org/testing/testScripts/testCase5.java
+++ src/org/testing/testScripts/testCase5.java
@@ -1,39 +1,13 @@
 package org.testing.testScripts;
 
-import java.io.IOException;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.remote.server.commandhandler.GetLogsOfType;
-import org.testing.BasePackage.BaseClass;
-import org.testing.Pages.HomePage;
-import org.testing.Pages.LoginPage;
-import org.testing.Pages.LogoutPage;
-import org.testing.Pages.VideoPlayPage;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-
-public class testCase5 extends BaseClass{
-		
+public class testCase5{
+	
 	@Test(priority=4)
-	public void tc5() throws InterruptedException, IOException {
+	public void tc5() {
+		System.out.println("this is test case #5");
 		
-		LoginPage ln = new LoginPage(driver, pr);
-		ln.signIn();
-		
-		HomePage hp = new HomePage(driver, pr);
-		hp.clickOnWatch();
-		
-		VideoPlayPage vp = new VideoPlayPage();
-		vp.clickOnChannel();
-		
-		LogoutPage lp = new LogoutPage();
-		lp.logout();
-
 	}
 
 }
